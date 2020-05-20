f38652a3b35de79c25420ca7bb4e9568d5ad14e9
FJFehr <fhrfab001@myuct.ac.za>
2020-05-20 18:57:29 +0200

    Finished tutorial 3

--- src/main/scala/Tutorial_3.scala
+++ src/main/scala/Tutorial_3.scala
@@ -19,6 +19,7 @@ object Tutorial_3 extends App {
 
   val dsGroup = ui.createGroup("datasets")
 
+  // This grabs 3 faces and displays them
   val meshFiles = new java.io.File("datasets/testFaces/").listFiles.take(3)
   val (meshes, meshViews) = meshFiles.map(meshFile => {
     val mesh = MeshIO.readMesh(meshFile).get
@@ -27,4 +28,77 @@ object Tutorial_3 extends App {
   }) .unzip // take the tuples apart, to get a sequence of meshes and one of meshViews
 
 
+  //set the reference mesh
+  val reference = meshes(0) // face_0 is our reference
+
+  // Now any mesh, which is in correspondence with this reference, can be represented as a deformation field!
+  // This is simply the difference between the new mesh and the reference
+  val deformations : IndexedSeq[EuclideanVector[_3D]] = reference.pointSet.pointIds.map {
+    id =>  meshes(1).pointSet.point(id) - reference.pointSet.point(id)
+  }.toIndexedSeq
+
+  // A deformation field in 3D is both deformation vector and the reference points
+  val deformationField = DiscreteField[_3D, UnstructuredPointsDomain[_3D], EuclideanVector[_3D]](reference.pointSet, deformations)
+  // NB this is discrete finite set of points = domain
+
+  // This is the deformation field for point id =0
+  deformationField(PointId(0))
+  // res2: EuclideanVector[_3D] = EuclideanVector3D(
+  //   -0.031402587890625,
+  //   -0.24579620361328125,
+  //   4.780601501464844
+  // )
+
+  // visualise the deformation field in the UI
+  val deformationFieldView = ui.show(dsGroup, deformationField, "deformations")
+
+  // removes the face3 and then makes the reference see though. It shows all the deformations
+  meshViews(2).remove()
+  meshViews(0).opacity = 0.3
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Deformation fields over continuous domains: ////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // Now lets make the domain continuous by means of interpolation.
+  val interpolator = NearestNeighborInterpolator[_3D, EuclideanVector[_3D]]()
+  val continuousDeformationField : Field[_3D, EuclideanVector[_3D]] = deformationField.interpolate(interpolator)
+
+  // This is sext as its defined on the entire real numberline and evaluate any 3D point
+  continuousDeformationField(Point(-100,-100,-100))
+  // images tend to use linear or b-spline interpolators which are also good
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // The mean deformation and the mean mesh: ////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  val nMeshes = meshes.length
+
+  // Maps each id to the average deformation for that id returns the deformation field
+  val meanDeformations = reference.pointSet.pointIds.map( id => {
+
+    var meanDeformationForId = EuclideanVector(0, 0, 0)
+
+    // Loops through all meshes and gets the average
+    val meanDeformations = meshes.foreach (mesh => { // loop through meshes
+      val deformationAtId = mesh.pointSet.point(id) - reference.pointSet.point(id)
+      meanDeformationForId += deformationAtId * (1.0 / nMeshes)
+    })
+
+    meanDeformationForId
+  })
+
+  val meanDeformationField = DiscreteField[_3D, UnstructuredPointsDomain[_3D], EuclideanVector[_3D]](
+    reference.pointSet,
+    meanDeformations.toIndexedSeq
+  )
+
+  // We can now apply the deformation to every point of the reference mesh, to obtain the mean mesh.
+  val continuousMeanDeformationField = meanDeformationField.interpolate(interpolator)
+  val meanTransformation = Transformation((pt : Point[_3D]) => pt + continuousMeanDeformationField(pt))
+
+  val meanMesh = reference.transform(meanTransformation)
+
+  ui.show(dsGroup, meanMesh, "mean mesh")
 }
