836e9970e9386dd66f838f565f9343f7f3ad1d02
FJFehr <fhrfab001@myuct.ac.za>
2020-05-20 17:34:39 +0200

    I have added a the datasets to the folder, and a it to gitignore

--- src/main/scala/Tutorial_3.scala
+++ src/main/scala/Tutorial_3.scala
@@ -5,5 +5,26 @@ object Tutorial_3 extends App {
   20 May 2020
   * */
 
+  import scalismo.geometry._
+  import scalismo.common._
+  import scalismo.ui.api._
+  import scalismo.registration.Transformation
+
+  scalismo.initialize()
+  implicit val rng = scalismo.utils.Random(42)
+
+
+  val ui = ScalismoUI()
+  import scalismo.io.MeshIO
+
+  val dsGroup = ui.createGroup("datasets")
+
+  val meshFiles = new java.io.File("datasets/testFaces/").listFiles.take(3)
+  val (meshes, meshViews) = meshFiles.map(meshFile => {
+    val mesh = MeshIO.readMesh(meshFile).get
+    val meshView = ui.show(dsGroup, mesh, "mesh")
+    (mesh, meshView) // return a tuple of the mesh and the associated view
+  }) .unzip // take the tuples apart, to get a sequence of meshes and one of meshViews
+
 
 }
